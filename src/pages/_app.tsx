95af471eff94595410d61cfe2b2ccfacfc0bd4c9
Deepcryptodive <61325205+Deepcryptodive@users.noreply.github.com>
2023-05-14 04:11:08 +0100

    Update _app.tsx - change bg image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,9 +7,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <img
-        style={{ position: "absolute", right: 0, zIndex: -1 }}
-        src="/assets/sismo-landing-art.svg"
-        alt="sismo art"
+        style={{ position: "absolute", right: 0, zIndex: -1, top: 0, marginRight: 25, width: "40%", height: "50%" }}
+        src="/assets/theo-celebration.svg"
+        alt="theo celebration bg image"
       />
       <Component {...pageProps} />
     </>
