5b9805e5e8bf992a50fcdc02fd663690050ec8d5
Deepcryptodive <61325205+Deepcryptodive@users.noreply.github.com>
2023-05-14 01:23:40 +0100

    Update AirdropLevel2.s.sol - add custom groups to deploy script

--- script/AirdropLevel2.s.sol
+++ script/AirdropLevel2.s.sol
@@ -6,9 +6,9 @@ import "forge-std/console.sol";
 import {AirdropLevel2} from "contracts/AirdropLevel2.sol";
 
 contract DeployAirdropLevel2 is Script {
-    bytes16 public constant APP_ID = 0xf4977993e52606cfd67b7a1cde717069;
-    bytes16 public constant GROUP_ID = 0x311ece950f9ec55757eb95f3182ae5e2;
-    bytes16 public constant GROUP_ID_2 = 0x1cde61966decb8600dfd0749bd371f12;
+    bytes16 public constant APP_ID =  0x6aa6b65b0f51e64729bc06022e76127b;
+    bytes16 public constant GROUP_ID = 0x349d8bd135bd903a633464f9b303c902;
+    bytes16 public constant GROUP_ID_2 = 0xc6ae11a89cc5cf7a34dac84c7d7b8eff;
 
     function run() public {
         vm.startBroadcast();
